b53bbb06d94609bf9811d4d0f25bad8a25bb9055
most2bl <most2balsuez@gmail.com>
2021-02-22 15:29:49 +0200

    bitch

--- system/settings.py
+++ system/settings.py
@@ -119,8 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
