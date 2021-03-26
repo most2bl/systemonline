6a4127bcaee65cc007dd48267faabb146d07dff6
most2bl <most2balsuez@gmail.com>
2021-03-26 03:43:11 +0200

    static files works perfectly!

--- system/settings.py
+++ system/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = "/home/mwatrdvm/most2blroot/inhouse/"
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
